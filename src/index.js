ecb194d1ac7cfc3422cbf74e64972b69fecd4916
jairo3936 <jairo3936martinez@gmail.com>
2024-03-21 10:27:26 -0500

    jws

--- src/index.js
+++ src/index.js
@@ -1,67 +1,24 @@
-// import express from "express"
-// import { configDotenv } from "dotenv"
-// import cors from "cors"
-// import groupsRoutes from "./routes/groups.routes.js";
-// import loginRouter from "./routes/login.routes.js";
-// import publicationRouter from "./routes/publications.routes.js";
-
-
-// configDotenv()
-
-// const app = express();
-// app.use(cors())
-
-// app.get("/", (req, res) => {
-//   res.send("Backend funcionando!");
-// });
-
-// app.use(express.json())
-// app.use("/api", groupsRoutes, loginRouter, publicationRouter)
-
-
-// const {PORT} = process.env
-// app.listen(PORT, () => {
-//   console.log("Server running on", PORT )
-// })
-
-import express from "express";
-import { configDotenv } from "dotenv";
-import cors from "cors";
-import jwt from "jsonwebtoken";
+import express from "express"
+import { configDotenv } from "dotenv"
+import cors from "cors"
 import groupsRoutes from "./routes/groups.routes.js";
 import loginRouter from "./routes/login.routes.js";
 import publicationRouter from "./routes/publications.routes.js";
 
-configDotenv();
+
+configDotenv()
 
 const app = express();
-app.use(cors());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Backend funcionando!");
 });
 
-app.use(express.json());
+app.use(express.json())
+app.use("/api", groupsRoutes, loginRouter, publicationRouter)
 
-// Middleware de autenticación con JWT
-const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (token == null) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-};
-
-// Aplica el middleware de autenticación a las rutas que lo necesiten
-app.use("/api", authenticateToken, groupsRoutes);
-app.use("/api", authenticateToken, publicationRouter);
-
-const { PORT } = process.env;
+const {PORT} = process.env
 app.listen(PORT, () => {
-  console.log("Server running on", PORT );
-});
+  console.log("Server running on", PORT )
+})
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import * as loginControllers from "../controllers/login.controller.js"
-
+import { authenticateToken } from "../Middleware/auth.js";
 const loginRouter = Router();
 
-loginRouter.get("/getUser", loginControllers.getLogin);//(ok)
+loginRouter.get("/getUser",authenticateToken, loginControllers.getLogin);//(ok)
 loginRouter.post("/login", loginControllers.loginUser);//(ok)
-loginRouter.post('/create', loginControllers.createUser);//(ok)
-loginRouter.delete('/delete/:id', loginControllers.deleteUser);//(ok)
-loginRouter.put("/update/:id", loginControllers.updateUser);
+loginRouter.post('/create',authenticateToken, loginControllers.createUser);//(ok)
+loginRouter.delete('/delete/:id',authenticateToken, loginControllers.deleteUser);//(ok)
+loginRouter.put("/update/:id",authenticateToken, loginControllers.updateUser);
 
 export default loginRouter;
